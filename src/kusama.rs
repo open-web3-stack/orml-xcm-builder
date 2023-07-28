2d63894d4c0ae3671c7ae7b9cb5f64c064730983
Gabriel Facco de Arruda <arrudagates@gmail.com>
2023-07-27 17:34:41 -0700

    Initial commit: Add Tinkernet Multisig configs, set version to 0.9.43

---
+++ src/kusama.rs
@@ -0,0 +1,2 @@
+pub mod tinkernet_multisigs;
+pub use tinkernet_multisigs::{TinkernetMultisigAsAccountId, TinkernetMultisigAsNativeOrigin};
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+// Types specific to Kusama and it's child parachains.
+pub mod kusama;
+
+// Types specific to Kusama and it's child parachains.
+pub mod polkadot;
